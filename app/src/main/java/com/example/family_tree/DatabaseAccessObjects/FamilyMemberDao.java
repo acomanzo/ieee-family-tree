c83440d55bc74964c9c4e588e0751824ded467f1
acomanzo <ansonih17@gmail.com>
2021-03-10 18:17:55 -0500

    create family member dao

---
+++ app/src/main/java/com/example/family_tree/DatabaseAccessObjects/FamilyMemberDao.java
@@ -0,0 +1,23 @@
+package com.example.family_tree.DatabaseAccessObjects;
+
+import com.example.family_tree.Models.FamilyMember;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+public interface FamilyMemberDao {
+
+    @Insert
+    void insert(FamilyMember familyMember);
+
+    @Query("DELETE FROM FamilyMember")
+    void deleteAll();
+
+    @Query("SELECT * FROM FamilyMember")
+    List<FamilyMember> getAllFamilyMembers();
+
+}
